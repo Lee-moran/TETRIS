38a86fe1443c825e016a0be323e41c3a03d545ad
Lee-moran <leemoran06@hotmail.com>
2023-04-05 14:29:36 +0000

    initial commit game set up tetris shapes

---
+++ app.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    // code goes here
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+    //The Tetrominoes ie the shapes 
+    const lTetrominoes = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+
+    ]
+
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+      ]
+
+      const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+      ]
+    
+      const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+      ]
+    
+      const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+      ]
+
+      const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    
+})
